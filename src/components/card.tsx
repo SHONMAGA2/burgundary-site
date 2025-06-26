2bcc375c71a8bf1276199d651d0e67df915b1dd5
SHONMAGA1 <ivynemuzenda9@gmail.com>
2025-06-26 13:04:48 +0000

    Describe what you changed

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,20 +1,17 @@
-
-
 type cardProp = {
-name:string,
-text:string,
-price:number,
-description?:string
+  name: string;
+  text: string;
+  price: number;
+  description?: string;
+};
 
-}
-
-export default function Card({name,text,price,description}:cardProp){
- return (
-  <div>
-   <h2> {name} </h2>
-   <p>price:R{price}</p>
-    <p> {text} </p>
-    {description && <p>{description}</p>}
-   </div>
-)
+export default function Card({ name, text, price, description }: cardProp) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>price: R{price}</p>
+      <p>{text}</p>
+      {description && <p>{description}</p>}
+    </div>
+  );
 }
