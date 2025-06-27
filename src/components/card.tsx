7284bff1e6f9fecbf3b6597ff4598907d26adb8e
SHONMAGA1 <ivynemuzenda9@gmail.com>
2025-06-27 09:49:41 +0000

    renewed website

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,16 +1,17 @@
-type cardProp = {
+type CardProps = {
   name: string;
   text: string;
   price?: number;
   description?: string;
 };
 
-export default function Card({ name, text, price, description }: cardProp) {
+export default function Card({ name, text, price, description }: CardProps) {
   return (
-<div className="flex justify-evenly text-center gap-20 mt-20 p-8 shadow-lg h-auto max-h-none min-h-0">
- <h2>{name}</h2>
-      {price !== undefined && <p>Price:  R{price}</p>}
+    <div className="flex justify-evenly text-center gap-20 mt-20 p-8 shadow-md">
+      <h2>{name}</h2>
+      {price !== undefined && <p>Price: R{price}</p>}
       <p>{text}</p>
       {description && <p>{description}</p>}
-</div>  );
+    </div>
+  );
 }
